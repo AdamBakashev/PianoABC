17bf9ef68615c0ce9520303db96d19bdd07d5900
Emmi Nguyen <s240776@th-ab.de>
2025-06-19 04:22:19 +0200

    klavier neugebaut, menü des challenge modus fast fertig gemacht. das angepasste klavier hat bis jetzt noch keinen code

---
+++ Skripte/challenge_mode.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+func _ready() -> void:
+	$ZurückZumHauptmenü.connect("pressed", Callable(self, "_on_back_pressed"))
+	$SpielenButton.connect("pressed", Callable(self, "_on_spielen_pressed"))
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://MainMenu.tscn")
+
+func _on_spielen_pressed():
+	get_tree().change_scene_to_file("res://ChallengePlay.tscn")
+#
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#pass # Replace with function body.
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
